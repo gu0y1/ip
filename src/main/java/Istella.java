f720d7372d50846960322c3916495954028268b3
gu0y1 <guoyi@comp.nus.edu.sg>
2025-02-14 10:09:02 +0800

    Implement Level-5: Handle Errors

--- src/main/java/Istella.java
+++ src/main/java/Istella.java
@@ -4,6 +4,7 @@ public class Istella {
     private static final int MAX_TASKS = 100;
     private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCount = 0;
+
     private static void printLine() {
         System.out.println("____________________________________________________________");
     }
@@ -18,47 +19,49 @@ public class Istella {
 
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            try {
+                processCommand(input);
+            } catch (IstellaException e) {
                 printLine();
-                System.out.println(" Bye. Hope to see you again soon!");
-                printLine();
-                break;
-            } else if (input.equals("list")) {
-                printLine();
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-                printLine();
-            } else if (input.startsWith("todo ")) {
-                String description = input.substring(5);
-                tasks[taskCount] = new Todo(description);
-                taskCount++;
-                printLine();
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[taskCount - 1]);
-                System.out.println(" Now you have " + taskCount + " tasks in the list.");
-                printLine();
-            } else if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                tasks[taskCount] = new Deadline(parts[0], parts[1]);
-                taskCount++;
-                printLine();
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[taskCount - 1]);
-                System.out.println(" Now you have " + taskCount + " tasks in the list.");
-                printLine();
-            } else if (input.startsWith("event ")) {
-                String[] parts = input.substring(6).split(" /from | /to ");
-                tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
-                taskCount++;
-                printLine();
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[taskCount - 1]);
-                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println(" OOPS!!! " + e.getMessage());
                 printLine();
             }
         }
+
         scanner.close();
+    }
+
+    private static void processCommand(String input) throws IstellaException {
+        if (input.equals("bye")) {
+            printLine();
+            System.out.println(" Bye. Hope to see you again soon!");
+            printLine();
+            System.exit(0);
+        } else if (input.equals("list")) {
+            printLine();
+            System.out.println(" Here are the tasks in your list:");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+            printLine();
+        } else if (input.startsWith("todo ")) {
+            String description = input.substring(5).trim();
+            if (description.isEmpty()) {
+                throw new IstellaException("The description of a todo cannot be empty.");
+            }
+            tasks[taskCount] = new Todo(description);
+            taskCount++;
+            printTaskAdded();
+        } else {
+            throw new IstellaException("I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    private static void printTaskAdded() {
+        printLine();
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + tasks[taskCount - 1]);
+        System.out.println(" Now you have " + taskCount + " tasks in the list.");
+        printLine();
     }
 }
